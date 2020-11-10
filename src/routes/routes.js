4c961d44b3db24de21c016ac0701081ccf5b14fa
Marcelo <marcelosen4@gmail.com>
2020-11-10 16:51:23 -0300

    agregado rutas principales con sus funciones que son importadas desde controllers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
+const { renderIndex, renderNewEntry, createNewEntry } = require('../controllers/controller');
 
+router.get('/', renderIndex);
+
+router.get('/new-entry', renderNewEntry);
+
+router.post('/new-entry', createNewEntry);
 
 module.exports = router;
